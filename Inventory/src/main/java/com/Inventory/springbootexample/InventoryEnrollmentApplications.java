93f06f9f58e7ae050c6a7c6f2d8bf1546fafa5a4
Saurya <sauryathome@gmail.com>
2019-07-16 08:04:50 +0530

    On branch branch1  Changes to be committed:

--- Inventory/src/main/java/com/Inventory/springbootexample/InventoryEnrollmentApplications.java
+++ Inventory/src/main/java/com/Inventory/springbootexample/InventoryEnrollmentApplications.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class InventoryEnrollmentApplications {  
-	 
+//test	 
 	public static void main(String[] args) {  
 				
 		SpringApplication.run(InventoryEnrollmentApplications.class, args);
